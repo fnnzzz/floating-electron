4a82665f525e36096368ec075402538475b81047
Vlad Feninets <v.feninets@gmail.com>
2017-03-06 13:10:58 +0200

    init

---
+++ main.js
@@ -0,0 +1,29 @@
+const { app, BrowserWindow, ipcMain, ipcRenderer } = require('electron')
+const path = require('path')
+const url = require('url')
+
+
+let win
+
+function createWindow() {
+	win = new BrowserWindow({ width: 800, height: 450, frame: false, alwaysOnTop: true })
+	win.loadURL(url.format({
+		pathname: path.join(__dirname, 'index.html'),
+		protocol: 'file:',
+		slashes: true
+	}))
+}
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', () => {
+	if (process.platform !== 'darwin') {
+		app.quit()
+	}
+})
+
+app.on('activate', () => {
+	if (win === null) {
+		createWindow()
+	}
+})
